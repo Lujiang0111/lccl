577349af435c481bc931bebeab9edb58d49dfba1
Lujiang <lujiang0111@gmail.com>
2024-04-18 17:08:03 +0800

    添加fmt和test_fmt

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,5 +1,28 @@
-﻿int main(int argc, char **argv)
-{
+﻿#include <cstring>
+#include <iostream>
+#include "test_fmt.h"
 
+enum class TestTypes
+{
+    kTestFmt = 0,
+};
+
+int main(int argc, char **argv)
+{
+    if (argc <= 1)
+    {
+        std::cerr << "too less args";
+    }
+
+    TestTypes test_type = static_cast<TestTypes>(atoi(argv[1]));
+    switch (test_type)
+    {
+    case TestTypes::kTestFmt:
+        TestFmt(argc, argv);
+        break;
+    default:
+        break;
+    }
+    
     return 0;
 }
---
+++ test/src/test_fmt.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include "lccl/fmt.h"
+#include "test_fmt.h"
+
+void TestFmt(int argc, char **argv)
+{
+    std::vector<std::string> ans;
+
+    ans.push_back(fmt::format("1:{} 2:{} 3:{}", 1, 2LL, "3"));
+    ans.push_back(fmt::format("123"));
+
+    for (size_t i = 0; i < ans.size(); ++i)
+    {
+        std::cout << i << ": " << ans[i] << std::endl;
+    }
+}
