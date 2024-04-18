c4af58bb84b7f81c324ae6645c63875b5988ccbb
Lujiang <lujiang0111@gmail.com>
2024-04-18 14:37:45 +0800

    添加基础框架

---
+++ src/lccl.cpp
@@ -0,0 +1,26 @@
+﻿#include "lccl.h"
+
+#ifndef VERSION_X
+#define VERSION_X 1
+#endif
+
+#ifndef VERSION_Y
+#define VERSION_Y 0
+#endif
+
+#ifndef VERSION_Z
+#define VERSION_Z 0
+#endif
+
+#define STRINGIFY(x) #x
+#define GET_VERSION(x, y, z) STRINGIFY(x) "." STRINGIFY(y) "." STRINGIFY(z)
+
+LCCL_BEGIN_NAMESPACE
+
+const char *GetVersion()
+{
+    const char *version = GET_VERSION(VERSION_X, VERSION_Y, VERSION_Z);
+    return version;
+}
+
+LCCL_END_NAMESPACE
