bd7fcf25a9792c20a0d0d2aa2784d11385873cfb
Lujiang <lujiang0111@gmail.com>
2024-04-18 16:58:12 +0800

    添加fmt

---
+++ include/lccl/fmt.h
@@ -0,0 +1,12 @@
+﻿#ifndef LCCP_INCLUDE_LCCL_FMT_H_
+#define LCCP_INCLUDE_LCCL_FMT_H_
+
+#ifndef FMT_HEADER_ONLY
+#define FMT_HEADER_ONLY
+#endif
+
+#include "lccl/fmt/color.h"
+#include "lccl/fmt/core.h"
+#include "lccl/fmt/format.h"
+
+#endif // !LCCP_INCLUDE_LCCL_FMT_H_
