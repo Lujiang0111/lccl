f318dac5a4c3efde9c0ebde5835a11ed167fb068
lujiang <lujiang0111@gmail.com>
2025-02-14 10:12:41 +0800

    修改拼写错误

--- include/lccl/fmt.h
+++ include/lccl/fmt.h
@@ -1,5 +1,5 @@
-﻿#ifndef LCCP_INCLUDE_LCCL_FMT_H_
-#define LCCP_INCLUDE_LCCL_FMT_H_
+﻿#ifndef LCCL_INCLUDE_LCCL_FMT_H_
+#define LCCL_INCLUDE_LCCL_FMT_H_
 
 #ifndef FMT_HEADER_ONLY
 #define FMT_HEADER_ONLY
@@ -10,4 +10,4 @@
 #include "lccl/fmt/core.h"
 #include "lccl/fmt/format.h"
 
-#endif // !LCCP_INCLUDE_LCCL_FMT_H_
+#endif // !LCCL_INCLUDE_LCCL_FMT_H_
