e9b491f8c20f780320a26137bf9f4ed3228cf6c0
lujiang <lujiang0111@gmail.com>
2025-03-14 08:50:36 +0800

    更改为官方推荐的用法

---
+++ include/lccl/utils/yaml.h
@@ -0,0 +1,8 @@
+﻿#ifndef LCCL_INCLUDE_UTILS_YAML_H_
+#define LCCL_INCLUDE_UTILS_YAML_H_
+
+#ifndef _RYML_SINGLE_HEADER_AMALGAMATED_HPP_
+#include "rapidyaml/rapidyaml.hpp"
+#endif
+
+#endif // !LCCL_INCLUDE_UTILS_YAML_H_
