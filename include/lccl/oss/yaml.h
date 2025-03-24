8826441ce619c87d6288c63b643fe2251fa6db7b
lujiang <lujiang0111@gmail.com>
2025-03-24 16:45:41 +0800

    添加C4ToString接口

--- include/lccl/oss/yaml.h
+++ include/lccl/oss/yaml.h
@@ -1,8 +1,15 @@
 ﻿#ifndef LCCL_INCLUDE_LCCL_OSS_YAML_H_
 #define LCCL_INCLUDE_LCCL_OSS_YAML_H_
 
+#include <string>
+
 #ifndef _RYML_SINGLE_HEADER_AMALGAMATED_HPP_
 #include "lccl/oss/rapidyaml/rapidyaml.hpp"
 #endif
+
+inline std::string C4ToString(const c4::csubstr &str)
+{
+    return std::string(str.str, str.len);
+}
 
 #endif // !LCCL_INCLUDE_LCCL_OSS_YAML_H_
