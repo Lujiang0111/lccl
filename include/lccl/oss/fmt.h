e7efc36964b8251638a92f0cb8f2a058d3a5cb9f
Lujiang <lujiang0111@gmail.com>
2025-05-16 09:59:29 +0800

    不判断异常

--- include/lccl/oss/fmt.h
+++ include/lccl/oss/fmt.h
@@ -5,6 +5,14 @@
 #define FMT_HEADER_ONLY
 #endif
 
+#ifndef FMT_USE_EXCEPTIONS
+#define FMT_USE_EXCEPTIONS 0
+#endif
+
+#ifndef FMT_USE_WRITE_CONSOLE
+#define FMT_USE_WRITE_CONSOLE 1
+#endif
+
 #include "lccl/oss/fmt/chrono.h"
 #include "lccl/oss/fmt/color.h"
 #include "lccl/oss/fmt/core.h"
