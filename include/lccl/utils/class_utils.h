9003df3159a310643de1886b420c18039e28358f
Lujiang <lujiang0111@gmail.com>
2025-05-30 11:20:07 +0800

    添加class_utils

---
+++ include/lccl/utils/class_utils.h
@@ -0,0 +1,31 @@
+﻿#ifndef LCCL_INCLUDE_LCCL_UTILS_CLASS_UTILS_H_
+#define LCCL_INCLUDE_LCCL_UTILS_CLASS_UTILS_H_
+
+#include <utility>
+#include "lccl.h"
+
+LCCL_BEGIN_NAMESPACE
+
+template <typename T, typename... Args>
+std::shared_ptr<T> MakeSharedProtected(Args &&...args)
+{
+    struct Enabler : public T
+    {
+        Enabler(Args &&... args) : T(std::forward<Args>(args)...) {}
+    };
+    return std::make_shared<Enabler>(std::forward<Args>(args)...);
+}
+
+template <typename T, typename Allocator, typename... Args>
+std::shared_ptr<T> AllocateSharedProtected(const Allocator &alloc, Args &&...args)
+{
+    struct Enabler : public T
+    {
+        Enabler(Args &&... args) : T(std::forward<Args>(args)...) {}
+    };
+    return std::allocate_shared<Enabler>(alloc, std::forward<Args>(args)...);
+}
+
+LCCL_END_NAMESPACE
+
+#endif // !LCCL_INCLUDE_LCCL_UTILS_CLASS_UTILS_H_
